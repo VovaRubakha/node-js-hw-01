058c4bc0ca1ab4cfa89b1496445861f2f8abc343
VovaRubakha <97684717+VovaRubakha@users.noreply.github.com>
2022-08-01 19:47:13 +0300

    init files, end tasks

---
+++ README.md
@@ -0,0 +1,11 @@
+list
+https://monosnap.com/file/crojn2lPjMR1HRTqcLBmJV0LdhKLE7
+
+get
+https://monosnap.com/file/vMU5ff7sZbQsPEjVTjA7WOucWXdeCZ
+
+add
+https://monosnap.com/file/dMlW1l4YOd0TatWzDp2nzj7lCQOYS0
+
+remove
+https://monosnap.com/file/KssbXB7s4nXhi9HehXIIDraCzkheUQ
---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require("fs/promises");
+const path = require("path");
+const ObjectID = require("bson-objectid");
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+const listContacts = async () => {
+  const result = await fs.readFile(contactsPath);
+  return JSON.parse(result);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = await contacts.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
+  }
+  return result;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(idx, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: ObjectID(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
